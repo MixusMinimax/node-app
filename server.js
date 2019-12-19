0531b6ca7e8d30177e32f97765a885c55d868487
baxiBarometer <maxi.barmetler@gmail.com>
2019-12-19 13:52:48 +0100

    register

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.get("/", (req, res) => {
     res.render("index.ejs")
 })
 
+app.get("/login", (req, res) => {
+    res.render("login.ejs")
+})
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs")
+})
+
+app.post("/register", (req, res) => {
+    
+})
+
+// start
+
 app.listen(port, () => {
     console.log("app running")
 })
